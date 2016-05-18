aaeed6dcb05be75b8860619d92b899553144ab55
xolf <webmaster@xolf.info>
2016-05-18 23:10:48 +0100

    Improved Name declaring for documents

--- src/Document.php
+++ src/Document.php
@@ -61,13 +61,9 @@ class Document
      */
     private function setName($name)
     {
-        $this->_name = $name;
-        $this->_name = str_replace('/', '', $this->_name);
-        $this->_name = str_replace('?', '', $this->_name);
-        $this->_name = str_replace('<', '', $this->_name);
-        $this->_name = str_replace('>', '', $this->_name);
-        $this->_name = str_replace(':', '-', $this->_name);
-        $this->_name = str_replace('.', '-', $this->_name);
+        $this->_name = str_replace(['/','?','>','<'], '', $this->_name);
+        $this->_name = str_replace([':','.'], '-', $this->_name);
+        $this->_name = str_replace([' '], '+', $this->_name);
     }
 
     /**
